7236040a2596521419508bb1deab2144622e6ec0
Liza Shankar <>
2021-03-09 16:55:17 -0500

    switch to mp3

--- index.js
+++ index.js
@@ -14,15 +14,10 @@ client.once('ready', () => {
 
 client.on('message', async message => {
   if (!message.guild) return;
-  if (message.content === `${prefix}hits`) {
+  if (message.content === `${prefix}big`) {
   if (message.member.voice.channel) {
-
-      const ytdl = require('ytdl-core');
-
       const connection = await message.member.voice.channel.join();
-      const stream = ytdl('https://youtu.be/5JjKm8DBq9U', { filter: 'audioonly' });
-			const dispatcher = connection.play(stream);
-
+      const dispatcher = connection.play('greatesthits.mp3');
       dispatcher.on('finish', () => message.member.voice.channel.leave());
     } else {
       message.reply('you need to join a voice channel first!');
