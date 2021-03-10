541dd165a3303b08fd5950a168db4b9bb80b007e
Liza Shankar <>
2021-03-09 21:10:48 -0500

    individual mp3s

--- index.js
+++ index.js
@@ -14,10 +14,75 @@ client.once('ready', () => {
 
 client.on('message', async message => {
   if (!message.guild) return;
-  if (message.content === `${prefix}hits`) {
+  if (message.content === `${prefix}br`) {
   if (message.member.voice.channel) {
       const connection = await message.member.voice.channel.join();
-      const dispatcher = connection.play('greatesthits.mp3');
+      const dispatcher = connection.play('mp3/bohemianrhapsody.mp3');
+      dispatcher.on('finish', () => message.member.voice.channel.leave());
+    } else {
+      message.reply('you need to join a voice channel first!');
+    }
+  }
+});
+
+client.on('message', async message => {
+  if (!message.guild) return;
+  if (message.content === `${prefix}dsmn`) {
+  if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+      const dispatcher = connection.play('mp3/dontstopmenow.mp3');
+      dispatcher.on('finish', () => message.member.voice.channel.leave());
+    } else {
+      message.reply('you need to join a voice channel first!');
+    }
+  }
+});
+
+client.on('message', async message => {
+  if (!message.guild) return;
+  if (message.content === `${prefix}goflb`) {
+  if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+      const dispatcher = connection.play('mp3/goodoldfashionedloverboy.mp3');
+      dispatcher.on('finish', () => message.member.voice.channel.leave());
+    } else {
+      message.reply('you need to join a voice channel first!');
+    }
+  }
+});
+
+client.on('message', async message => {
+  if (!message.guild) return;
+  if (message.content === `${prefix}kq`) {
+  if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+      const dispatcher = connection.play('mp3/killerqueen.mp3');
+      dispatcher.on('finish', () => message.member.voice.channel.leave());
+    } else {
+      message.reply('you need to join a voice channel first!');
+    }
+  }
+});
+
+client.on('message', async message => {
+  if (!message.guild) return;
+  if (message.content === `${prefix}stl`) {
+  if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+      const dispatcher = connection.play('mp3/somebodytolove.mp3');
+      dispatcher.on('finish', () => message.member.voice.channel.leave());
+    } else {
+      message.reply('you need to join a voice channel first!');
+    }
+  }
+});
+
+client.on('message', async message => {
+  if (!message.guild) return;
+  if (message.content === `${prefix}ymbf`) {
+  if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+      const dispatcher = connection.play('mp3/youremybestfriend.mp3');
       dispatcher.on('finish', () => message.member.voice.channel.leave());
     } else {
       message.reply('you need to join a voice channel first!');
